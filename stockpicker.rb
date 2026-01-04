b17f1b8439aad476e04b45f5e8ba5d29d904a958
José Vinicius <joseviniciusfsarmento@gmail.com>
2026-01-04 15:19:36 -0300

    stockPrice

---
+++ stockpicker.rb
@@ -0,0 +1,26 @@
+def stockPicker(arr)
+  min_day = 0
+  min_price = arr.first
+  best_day = []
+  max_profit = 0
+
+
+  arr.each_with_index do |price,day|
+    if price < min_price
+      min_day =day
+      min_price = price
+      next
+    end
+    
+    price = price - min_price
+
+    if price > max_profit
+      max_profit = price
+      best_day = [min_day,day]
+    end
+
+  end
+  best_day
+end
+
+puts stockPicker([17,3,6,9,15,8,6,1,10])
